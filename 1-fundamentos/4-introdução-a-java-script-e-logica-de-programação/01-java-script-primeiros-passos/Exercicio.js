456b7b49c8290700b15402bcd36e7a98fef7b5a5
Josielle Vaz <josiellevaz.adm@gmail.com>
2021-12-29 19:47:46 -0300

    Exercicio4

---
+++ 1-fundamentos/4-introdução-a-java-script-e-logica-de-programação/01-java-script-primeiros-passos/Exercicio.js
@@ -0,0 +1,30 @@
+//Exercicio 1
+const1=5
+const2=10
+console.log(const1+const2)   
+console.log(const1-const2)
+console.log(const1*const2)
+console.log(const1/const2)
+console.log(const1%const2)
+
+//Exercicio 2
+Valor1=70
+Valor2=140
+if(Valor1>Valor2) { 
+    console.log("Valor1 é maior que o Valor2");
+} else {
+    console.log("Valor2 é maior que o Valor1");
+};
+
+//Exercicio 3
+Theo=2
+Helena=3
+Benicio=4
+if(Theo>Helena && Theo>Benicio) {
+    console.log("Theo é mais velho");
+} else if(Helena>Theo && Helena>Benicio) {
+    console.log("Helena é mais velha");
+} else {
+    console.log("Benicio é o mais velho");
+}
+
